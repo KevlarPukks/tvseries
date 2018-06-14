18bf4dbaad10d3e60ce73ae37c0a92fb5e96a92f
pukks <pukks88@googlemail.com>
2018-06-14 13:17:50 +0100

    added toggle to navbar

--- index.php
+++ index.php
@@ -26,6 +26,14 @@
 
 <div class="w3-bar w3-theme-d5">
     <button class="w3-bar-item w3-button" id="home">Home</button>
+    <button class="w3-bar-item w3-button w3-hide-small" id="music" >Music</button>
+    <button class="w3-bar-item w3-button w3-hide-small" id="tvSeries" >Tv Series</button>
+    <button class="w3-bar-item w3-button w3-hide-small" id="pukkaStudios" >Pukka Studios</button>
+    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="navToggle()">&#9776;</a>
+</div>
+
+<div id="nav" class="w3-bar-block w3-hide w3-hide-large w3-hide-medium w3-theme-d5">
+    <button class="w3-bar-item w3-button" id="home">Home</button>
     <button class="w3-bar-item w3-button" id="music" >Music</button>
     <button class="w3-bar-item w3-button" id="tvSeries" >Tv Series</button>
     <button class="w3-bar-item w3-button" id="pukkaStudios" >Pukka Studios</button>
--- js/main.js
+++ js/main.js
@@ -25,3 +25,12 @@ insertOnClick($('#empire'),empire);
     });
 }
 });
+
+function navToggle() {
+    var x = document.getElementById("nav");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+};
